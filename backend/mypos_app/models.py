d1cfdae4a4f7acd3bd0f4ef07dad4295ce686ee8
muharifs <muh.arifs1505@gmail.com>
2026-01-30 22:17:42 +0700

    fixing

--- backend/mypos_app/models.py
+++ backend/mypos_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+class UserProfile(models.Model):
+    ROLE_CHOICES = [
+        ('master', 'Master'),
+        ('admin', 'Admin'),
+        ('cashier', 'Cashier'),
+    ]
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='cashier')
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
+    
+# Automatically create new UserProfile is cashier role is selected
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance, role='cashier')
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
@@ -6,11 +7,26 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
+=======
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProtectedRoute from "@/routes/ProtectedRoute";
+
+import LoginPage from "@/auth/login-form";
+import Master from "@/pages/master";
+import Cashier from "@/pages/cashier";
+import Admin from "@/pages/cashier";
+
+function App() {
+>>>>>>> parent of 0e896bb (Revert "login form")
   return (
     <>
       <BrowserRouter>
         <Routes>
+<<<<<<< HEAD
           <Route path="/" element={<LoginPage />} />
+=======
+          <Route path="/login" element={<LoginPage />} />
+>>>>>>> parent of 0e896bb (Revert "login form")
           <Route
             path="/cashier"
             element={
