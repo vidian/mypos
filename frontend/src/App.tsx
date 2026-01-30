5f0a923461aaf09f6b8cc024c6528cc3f139bafc
muharifs <muh.arifs1505@gmail.com>
2026-01-30 22:45:04 +0700

    create views.py

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
-function App() {
-  const [count, setCount] = useState(0);
-
-=======
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProtectedRoute from "@/routes/ProtectedRoute";
 
@@ -17,16 +7,11 @@ import Cashier from "@/pages/cashier";
 import Admin from "@/pages/cashier";
 
 function App() {
->>>>>>> parent of 0e896bb (Revert "login form")
   return (
     <>
       <BrowserRouter>
         <Routes>
-<<<<<<< HEAD
           <Route path="/" element={<LoginPage />} />
-=======
-          <Route path="/login" element={<LoginPage />} />
->>>>>>> parent of 0e896bb (Revert "login form")
           <Route
             path="/cashier"
             element={
