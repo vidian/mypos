456d995b593df601fba44f88e65b83f79c521d15
muharifs <muh.arifs1505@gmail.com>
2026-01-31 00:47:44 +0700

    t

--- frontend/src/pages/cashier.tsx
+++ frontend/src/pages/cashier.tsx
@@ -1,7 +1,3 @@
 export default function Cashier() {
-<<<<<<< HEAD
-  return <p>Cashier Page 2 tes</p>;
-=======
   return <p>Cashier Page</p>;
->>>>>>> parent of 0e896bb (Revert "login form")
 }
--- frontend/src/routes/ProtectedRoute.tsx
+++ frontend/src/routes/ProtectedRoute.tsx
@@ -9,14 +9,8 @@ export default function ProtectedRoute({ children, allow }: Props) {
   const token = localStorage.getItem("access");
   const role = localStorage.getItem("role");
 
-<<<<<<< HEAD
   if (!token) return <Navigate to="/" />;
   if (!role || !allow.includes(role as any)) return <Navigate to="/" />;
-=======
-  if (!token) return <Navigate to="/login" />;
-  if (!role || !allow.includes(role as any))
-    return <Navigate to="/login" />;
->>>>>>> parent of 0e896bb (Revert "login form")
 
   return children;
 }
