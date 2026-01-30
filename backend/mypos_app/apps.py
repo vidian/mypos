6edccaa79354a34dc38e1c0b5ff45919149ec774
muharifs <muh.arifs1505@gmail.com>
2026-01-30 22:45:44 +0700

    Update apps.py

--- backend/mypos_app/apps.py
+++ backend/mypos_app/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class MyposAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'mypos_app'
+
+    def ready(self):
+        import mypos_app.models  # noqa
